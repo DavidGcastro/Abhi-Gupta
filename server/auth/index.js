1efef9f6261347a8c71af598dda2ba0d5fcdd90b
David <davidgcastro93@gmail.com>
2020-10-21 20:35:23 -0400

    remove server dir

--- server/auth/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-//Map to api files
-// starting from /auth
-router.use('/login', require('./login'));
-router.use('/signup', require('./signup'));
-
-module.exports = router;
--- server/auth/signup.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-router.get('/', (req, res) => {
-  res.send('Signup');
-});
-
-module.exports = router;

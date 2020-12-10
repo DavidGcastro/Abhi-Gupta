d0d233599a417a63d4f91252cff3330ae5062442
David <davidgcastro93@gmail.com>
2020-12-10 01:00:22 -0500

    finished grid

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,58 +1,23 @@
 import React from 'react';
 import projectsTemplate from '../../template/projectsTemplate';
-const pubAdsCol1 = projectsTemplate.pubArr.slice(0, projectsTemplate.pubArr.length /2);
-const pubAdsCol2 = projectsTemplate.pubArr.slice(projectsTemplate.pubArr.length /2);
-const colBuilder = (arr, i) => {
-	return <div className='publicationsCol' key={i}>
-		{arr.map((pub, i) => {
-			return (
-				pub.link && pub.link.length ? 
-					<a key={i} className='pubTab pointer' href = {pub.link} target='_blank'>
-						<div className='pubTab-top'>
-							<span className='small grey light detail-color'>{pub.year}</span>
-							<span className='p  white  boldish'>{pub.title}</span>
-							<span className='p grey light spacer'>{pub.description}</span>
-						</div>
-						<span className='small link-color letter-spacer pubTab-link'>Read more</span>
-					</a> :
-					<div key={i} className='pubTab removeHoverEvents'>
-						<div className='pubTab-top'>
-							<span className='small grey light detail-color'>{pub.year}</span>
-							<span className='p boldish white'>{pub.title}</span>
-							<span className='p grey light spacer'>{pub.description}</span>
-						</div>
-					
-					</div>
-			);
-		})}
-	</div>;
-};
+
 const Projects = () => {
 	return (
 		<div className='component-parent big-spacer' id={projectsTemplate.title.toLowerCase()}>
 			<div className='component-wrapper'>
-				<div className='publications-parent'>
-					{
-						projectsTemplate && projectsTemplate.highlightedPub && projectsTemplate.highlightedPub.link.length ? 		
-							<a href = {projectsTemplate.highlightedPub.link} target='_blank' className ='publications-left'>
-								<div className='publications-highlight'>
-									<span className='small light detail-color'>{projectsTemplate.highlightedPub && projectsTemplate.highlightedPub.year}</span>
-									<span className='p boldish uppercase textCenter letter-spacer'>{projectsTemplate.highlightedPub && projectsTemplate.highlightedPub.title}</span>
+				<div className='projects-parent'>
+					{projectsTemplate.projectsArr && projectsTemplate.projectsArr.map((project, i) => {
+						return (
+							<a key={i} className='project-cell-parent pointer' href={project.link}>
+								<div className='project-top'>
+									<span className='small grey light detail-color'>{project.year}</span>
+									<span className='p  white  boldish'>{project.title}</span>
+									<span className='p grey light spacer'>{project.description}</span>
 								</div>
-								<span className='letter-spacer link-color small pubTab-link'>{projectsTemplate.highlightedPub && projectsTemplate.highlightedPub.link ? 'Read More' : ''}</span>
-							</a> : 	
-							<div className ='publications-left removeHoverEvents'>
-								<div className='publications-highlight'>
-									<span className='small light detail-color'>{projectsTemplate.highlightedPub && projectsTemplate.highlightedPub.year}</span>
-									<span className='p boldish textCenter letter-spacer'>{projectsTemplate.highlightedPub && projectsTemplate.highlightedPub.title}</span>
-									<div></div>
-								</div>
-							</div>
-					}
-					<div className ='publications-right'>
-						{colBuilder(pubAdsCol2)}
-						{colBuilder(pubAdsCol1)}
-					</div>
+								<span className='small link-color letter-spacer project-link'>Read more</span>
+							</a> 
+						);
+					})}
 				</div>
 			</div>
 		</div>);
--- template/projectsTemplate.js
+++ template/projectsTemplate.js
@@ -8,7 +8,7 @@ export default {
 		year: '2020',
 		link: 'google.com',
 	},
-	pubArr: [
+	projectsArr: [
 		{
 			title: 'A Self-Supervised Framework for Online Robot-Camera Calibration',
 			description: 'In preparation for RSS 2021',
@@ -36,6 +36,83 @@ export default {
 			year: '2020',
 			link: 'google.com',
 			contributors: ['Bohan Wu', 'Iretiayo Akinola', 'Abhi Gupta', 'Feng Xu', 'Jake Varley', 'David Watkins-Valls', 'Peter Allen']
+		},
+		{
+			title: 'A Self-Supervised Framework for Online Robot-Camera Calibration',
+			description: 'In preparation for RSS 2021',
+			contributors: ['Abhi Gupta', 'Jingxi Xu', 'Peter Allen', 'Shuran Song'],
+			link: 'google.com',
+			year: '2020',
+
+		},
+		{
+			title: 'Leveraging Uncertainty-Aware Dynamics Models to Refine Actor-Critic Methods',
+			description: 'In preparation for ICML 2021',
+			contributors: ['Robert Kwiatkowski', 'Abhi Gupta', 'Wonjun Son', 'Boyuan Chen', 'Hod Lipson'], 
+			link: 'google.com',
+			year: '2020',
+		},
+		{
+			title: 'SQUIRL: Robust and Efficient Learning from Video Demonstration of Long-Horizon Robotic Manipulation Tasks',
+			description: 'IROS 2020',
+			year: '2020',
+			link: 'google.com',
+		},
+		{
+			title: 'Generative Attention Learning: A General Framework for High-Performance Multi-fingered Grasping in Clutter',
+			description: 'Autonomous Robots 2020',
+			year: '2020',
+			link: 'google.com',
+			contributors: ['Bohan Wu', 'Iretiayo Akinola', 'Abhi Gupta', 'Feng Xu', 'Jake Varley', 'David Watkins-Valls', 'Peter Allen']
+		},
+		{
+			title: 'A Self-Supervised Framework for Online Robot-Camera Calibration',
+			description: 'In preparation for RSS 2021',
+			contributors: ['Abhi Gupta', 'Jingxi Xu', 'Peter Allen', 'Shuran Song'],
+			link: 'google.com',
+			year: '2020',
+
+		},
+		{
+			title: 'Leveraging Uncertainty-Aware Dynamics Models to Refine Actor-Critic Methods',
+			description: 'In preparation for ICML 2021',
+			contributors: ['Robert Kwiatkowski', 'Abhi Gupta', 'Wonjun Son', 'Boyuan Chen', 'Hod Lipson'], 
+			link: 'google.com',
+			year: '2020',
+		},
+		{
+			title: 'SQUIRL: Robust and Efficient Learning from Video Demonstration of Long-Horizon Robotic Manipulation Tasks',
+			description: 'IROS 2020',
+			year: '2020',
+			link: 'google.com',
+		},
+		{
+			title: 'Generative Attention Learning: A General Framework for High-Performance Multi-fingered Grasping in Clutter',
+			description: 'Autonomous Robots 2020',
+			year: '2020',
+			link: 'google.com',
+			contributors: ['Bohan Wu', 'Iretiayo Akinola', 'Abhi Gupta', 'Feng Xu', 'Jake Varley', 'David Watkins-Valls', 'Peter Allen']
+		},
+		{
+			title: 'A Self-Supervised Framework for Online Robot-Camera Calibration',
+			description: 'In preparation for RSS 2021',
+			contributors: ['Abhi Gupta', 'Jingxi Xu', 'Peter Allen', 'Shuran Song'],
+			link: 'google.com',
+			year: '2020',
+
+		},
+		{
+			title: 'Leveraging Uncertainty-Aware Dynamics Models to Refine Actor-Critic Methods',
+			description: 'In preparation for ICML 2021',
+			contributors: ['Robert Kwiatkowski', 'Abhi Gupta', 'Wonjun Son', 'Boyuan Chen', 'Hod Lipson'], 
+			link: 'google.com',
+			year: '2020',
+		},
+		{
+			title: 'SQUIRL: Robust and Efficient Learning from Video Demonstration of Long-Horizon Robotic Manipulation Tasks',
+			description: 'IROS 2020',
+			year: '2020',
+			link: 'google.com',
 		},
 	]
 };

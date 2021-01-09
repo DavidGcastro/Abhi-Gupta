e027d1277e69439c686633290f38ddfaf26bf01b
Abhi Gupta <Abhi@Abhis-MacBook.local>
2021-01-09 03:49:47 -0500

    completed projects

--- template/projectsTemplate.js
+++ template/projectsTemplate.js
@@ -10,109 +10,70 @@ export default {
 	},
 	projectsArr: [
 		{
-			title: 'A Self-Supervised Framework for Online Robot-Camera Calibration',
-			description: 'In preparation for RSS 2021',
-			contributors: ['Abhi Gupta', 'Jingxi Xu', 'Peter Allen', 'Shuran Song'],
-			link: 'google.com',
+			title: 'Reinforcement Learning Playground',
+			description: 'A library of barebones implementations of model-free and model-based algorithms, including REINFORCE, PPO, TRPO, SARSA, DQN, DDQN, ME-TRPO, and MBPO.',
 			year: '2020',
+			link: 'TODO'
+		},
+		{
+			title: 'Learning from EMG Synergies to Grasp Objects by Superquadric Representation',
+			description: 'A dataset of human grasps on a variety of YCB objects, including raw EMG signals and joint angles, for the purposes of building a rich latent space.',
+			year: '2019',
+			link: './projects/EMG_synergies.pdf'
+		},
+		{
+			title: 'Audiv',
+			description: 'A bot service within Facebook group chats that transcribes speech to text in real-time, enabling the hearing impaired to engage in live conversations.',
+			year: 'Columbia DevFest 2018',
+			link: 'www.github.com/ScrypticLabs/Audiv'
+		},
+		{
+			title: 'A Decentralized SSH and FTP Protocol',
+			description: 'A command-line interface for collaborating on projects without uploading confidential files over the network to a central server.',
+			link: 'www.github.com/ScrypticLabs/PubNub-Shell',
+			year: '2018',
 
 		},
 		{
-			title: 'Leveraging Uncertainty-Aware Dynamics Models to Refine Actor-Critic Methods',
-			description: 'In preparation for ICML 2021',
-			contributors: ['Robert Kwiatkowski', 'Abhi Gupta', 'Wonjun Son', 'Boyuan Chen', 'Hod Lipson'], 
-			link: 'google.com',
-			year: '2020',
+			title: 'Run Bird, Run!',
+			description: 'An iOS game that requires the player to move left or right as a bird to avoid being crushed by falling crates.',
+			year: '2017',
+			link: 'www.github.com/ScrypticLabs/Run-Bird-Run'
 		},
 		{
-			title: 'SQUIRL: Robust and Efficient Learning from Video Demonstration of Long-Horizon Robotic Manipulation Tasks',
-			description: 'IROS 2020',
-			year: '2020',
-			link: 'google.com',
+			title: 'Watson',
+			description: 'A cellular smart personal assistant that retrieves web-based information exclusively with SMS to answer your most pressing questions.',
+			year: 'Facebook Global Hackathon Finals 2016',
+			link: 'www.github.com/ScrypticLabs/Watson'
 		},
 		{
-			title: 'Generative Attention Learning: A General Framework for High-Performance Multi-fingered Grasping in Clutter',
-			description: 'Autonomous Robots 2020',
-			year: '2020',
-			link: 'google.com',
-			contributors: ['Bohan Wu', 'Iretiayo Akinola', 'Abhi Gupta', 'Feng Xu', 'Jake Varley', 'David Watkins-Valls', 'Peter Allen']
+			title: 'Gateway',
+			description: 'An iOS messaging application with built-in chat bots for spontaneuous or requested user assistance.',
+			year: 'MHacks 2016',
+			link: 'www.github.com/ScrypticLabs/Gateway'
 		},
 		{
-			title: 'A Self-Supervised Framework for Online Robot-Camera Calibration',
-			description: 'In preparation for RSS 2021',
-			contributors: ['Abhi Gupta', 'Jingxi Xu', 'Peter Allen', 'Shuran Song'],
-			link: 'google.com',
-			year: '2020',
-
+			title: 'A Walk-in Medical Clinic Patient Portal',
+			description: 'A portal that can be managed by health administrators and viewed by patients for information like waiting time to see the clinician.',
+			year: '2016',
+			link: 'www.github.com/ScrypticLabs/Wait-Scale',
 		},
 		{
-			title: 'Leveraging Uncertainty-Aware Dynamics Models to Refine Actor-Critic Methods',
-			description: 'In preparation for ICML 2021',
-			contributors: ['Robert Kwiatkowski', 'Abhi Gupta', 'Wonjun Son', 'Boyuan Chen', 'Hod Lipson'], 
-			link: 'google.com',
-			year: '2020',
+			title: 'Recognizing Sign Language with Leap Motion',
+			description: 'A Java Applet for classifying hand gestures as words in sign language using the Leap Motion controller.',
+			link: 'www.github.com/ScrypticLabs/Syne',
+			year: '2015',
 		},
 		{
-			title: 'SQUIRL: Robust and Efficient Learning from Video Demonstration of Long-Horizon Robotic Manipulation Tasks',
-			description: 'IROS 2020',
-			year: '2020',
-			link: 'google.com',
+			title: 'Eye-Tracking',
+			description: 'A novel communication device for MS, PPMS, and ALS patients that allows them to control their computer\'s operating system with just their eyes.',
+			link: 'www.github.com/ScrypticLabs/iWriter',
+			year: 'Canada-Wide Science Fair 2014',
 		},
 		{
-			title: 'Generative Attention Learning: A General Framework for High-Performance Multi-fingered Grasping in Clutter',
-			description: 'Autonomous Robots 2020',
-			year: '2020',
-			link: 'google.com',
-			contributors: ['Bohan Wu', 'Iretiayo Akinola', 'Abhi Gupta', 'Feng Xu', 'Jake Varley', 'David Watkins-Valls', 'Peter Allen']
-		},
-		{
-			title: 'A Self-Supervised Framework for Online Robot-Camera Calibration',
-			description: 'In preparation for RSS 2021',
-			contributors: ['Abhi Gupta', 'Jingxi Xu', 'Peter Allen', 'Shuran Song'],
-			link: 'google.com',
-			year: '2020',
-
-		},
-		{
-			title: 'Leveraging Uncertainty-Aware Dynamics Models to Refine Actor-Critic Methods',
-			description: 'In preparation for ICML 2021',
-			contributors: ['Robert Kwiatkowski', 'Abhi Gupta', 'Wonjun Son', 'Boyuan Chen', 'Hod Lipson'], 
-			link: 'google.com',
-			year: '2020',
-		},
-		{
-			title: 'SQUIRL: Robust and Efficient Learning from Video Demonstration of Long-Horizon Robotic Manipulation Tasks',
-			description: 'IROS 2020',
-			year: '2020',
-			link: 'google.com',
-		},
-		{
-			title: 'Generative Attention Learning: A General Framework for High-Performance Multi-fingered Grasping in Clutter',
-			description: 'Autonomous Robots 2020',
-			year: '2020',
-			link: 'google.com',
-			contributors: ['Bohan Wu', 'Iretiayo Akinola', 'Abhi Gupta', 'Feng Xu', 'Jake Varley', 'David Watkins-Valls', 'Peter Allen']
-		},
-		{
-			title: 'A Self-Supervised Framework for Online Robot-Camera Calibration',
-			description: 'In preparation for RSS 2021',
-			contributors: ['Abhi Gupta', 'Jingxi Xu', 'Peter Allen', 'Shuran Song'],
-			link: 'google.com',
-			year: '2020',
-
-		},
-		{
-			title: 'Leveraging Uncertainty-Aware Dynamics Models to Refine Actor-Critic Methods',
-			description: 'In preparation for ICML 2021',
-			contributors: ['Robert Kwiatkowski', 'Abhi Gupta', 'Wonjun Son', 'Boyuan Chen', 'Hod Lipson'], 
-			link: 'google.com',
-			year: '2020',
-		},
-		{
-			title: 'SQUIRL: Robust and Efficient Learning from Video Demonstration of Long-Horizon Robotic Manipulation Tasks',
-			description: 'IROS 2020',
-			year: '2020',
-			link: 'google.com',
+			title: 'Solar-Tracking',
+			description: 'An IoT hardware device that autonomously positions photovoltaic panels in the direction of the sun using photocells.',
+			year: 'Canada-Wide Science Fair 2013',
 		},
 	]
 };

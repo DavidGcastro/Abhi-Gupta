17a16452a8eab113a5b2d781ba2a2c68051bf00e
David <davidgcastro93@gmail.com>
2021-01-09 19:52:15 -0500

    news section

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import {replacePlaceholders} from '../../utils/replacePlaceholders';
 import LazyImage from '../HOC/LazyImage';
 import { AiOutlineMail, AiOutlineFileText, AiFillGithub } from 'react-icons/ai';
 import { SiGooglescholar } from 'react-icons/si';
-
+console.log(Object.keys(aboutTemplate));
 
 class About extends React.Component {
 	constructor(){
@@ -22,7 +22,7 @@ class About extends React.Component {
 							<LazyImage src = {aboutTemplate.img}  imageStyle='about-img'/>
 							<div className='about-contact'>
 								{
-									Object.keys(aboutTemplate[Object.keys(aboutTemplate)[5]]).map((contact, i) => {
+									Object.keys(aboutTemplate[Object.keys(aboutTemplate)[6]]).map((contact, i) => {
 										if(contact.toLowerCase() === 'email') {
 											return <a key={i} href={aboutTemplate[Object.keys(aboutTemplate)[5]][contact]}><AiOutlineMail  size={20}/></a>;
 										}
@@ -46,14 +46,21 @@ class About extends React.Component {
 							})}
 						</div>
 					</div>
-					<div className='flex-child about-second'>
+					<div className='flex-child about-section full-width spacer-mid'>
 						<span className='h2 header-color block'>{Object.keys(aboutTemplate)[3]}</span>
-						<span className='p subtext-color spacer-small light'>{aboutTemplate[Object.keys(aboutTemplate)[3]]}</span>
+						{aboutTemplate[Object.keys(aboutTemplate)[3]].map((bullet, i) => {
+							return <span className='p subtext-color spacer-small light' key={i}> <span style={{marginRight: '5px'}}className='white-emphasis bold'>&#8226;</span>{bullet}</span>;
+						})}
+						
 					</div>
-					<div className='flex-child full-width about-third'>
+					<div className='flex-child about-section spacer-mid'>
 						<span className='h2 header-color block'>{Object.keys(aboutTemplate)[4]}</span>
+						<span className='p subtext-color spacer-small light'>{aboutTemplate[Object.keys(aboutTemplate)[4]]}</span>
+					</div>
+					<div className='flex-child full-width spacer-mid'>
+						<span className='h2 header-color block'>{Object.keys(aboutTemplate)[5]}</span>
 						<div className='about-list-container'>
-							{aboutTemplate[Object.keys(aboutTemplate)[4]].map((topic, i) => {
+							{aboutTemplate[Object.keys(aboutTemplate)[5]].map((topic, i) => {
 								return (
 									<span className='p emphasis-color' key={i}>{topic}</span>
 								);

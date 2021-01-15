1de5872e9a22b835581b36d21e9c500e1ee42c8e
David <davidgcastro93@gmail.com>
2021-01-14 20:20:16 -0500

    adjusted spacing in about

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ import LazyImage from '../HOC/LazyImage';class About extends React.Component {
 	}
 	render() {	
 		return (
-			<div className='component-parent' id={aboutTemplate.title.toLowerCase()} style={{marginTop: this.props.padHeight}}>
+			<div className='component-parent' id={aboutTemplate.title.toLowerCase()} style={{marginTop: this.props.padHeight || 50}}>
 				<div className='component-wrapper'>
 					<div className='about-main-parent'>
 						<div className='about-img-wrapper'>
@@ -32,7 +32,7 @@ import LazyImage from '../HOC/LazyImage';class About extends React.Component {
 					<div className='flex-child about-section-parent'>
 						{aboutTemplate.sections && aboutTemplate.sections.map((sections, i) => {
 							return (
-								<div className='about-section full-width spacer-mid' key={i}>
+								<div className='about-section full-width' key={i}>
 									<span className='h2 header-color block'>{sections.title}</span>
 									{sections.content && sections.content.map((content, i) => {
 										return <span className={`p subtext-color light ${sections.type === 'list'? '' : 'spacer-small'}`} key={i}> {sections.type === 'list' ? <span style={{marginRight: '5px'}}className='white-emphasis bold'>&#8226;</span> : ''}{content}</span>;
